ff3b3f1a117e3aece2c23cb316da5346e9f1b742
Sam Nielsen <sammn721@gmail.com>
2021-05-29 20:43:15 -0700

    added some test questions to js file

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,42 @@
 var timerEl = document.querySelector(".timer");
-var remainingTime = 100;
+var remainingTime = 101;
+var timerInterval;
 
 function setTimer() {
-  var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
     remainingTime--;
     timerEl.textContent = "TIME: " + remainingTime;
 
+    // if(userChoice == false) {
+    //     remainingTime = (remainingTime - 10);
+    // }
+
     if(remainingTime === 0) {
-      clearInterval(timerInterval);
-    }
+        clearInterval(timerInterval);
+      }
 
   }, 1000);
 }
+
+var questions = [
+    {
+        quest: "Commonly used data types DO NOT include:",
+        ans: {
+            strings,
+            booleans,
+            alerts,
+            numbers
+        },
+        correct: alerts
+    },
+    {
+        quest: "The condition in an if/else statement is enclosed within ___.",
+        ans: {
+            
+        }
+    }
+]
+
+
 
 setTimer();
