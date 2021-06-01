54467df7b5f884a87b4ce2cbd0137ca416445914
Sam Nielsen <sammn721@gmail.com>
2021-05-31 22:50:18 -0700

    got my questions to appear, as well as buttons to ago to next question, and event listener that matches with element

--- assets/script.js
+++ assets/script.js
@@ -3,42 +3,51 @@ var score = 101;
 var timerInterval;
 var startScreenEl = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start-button");
-var questionsEl = document.querySelector("#questions");
-var buttonContainer = document.querySelector("#button-container")
-var endScreenEl = document.querySelector("#end-screen")
+var questionsEl = document.querySelector("#question-element");
+var questionText = document.querySelector("#question-text");
+var buttonContainer = document.querySelector("#button-container");
+var buttonA = document.querySelector("#buttonA");
+var buttonB = document.querySelector("#buttonB");
+var buttonC = document.querySelector("#buttonC");
+var buttonD = document.querySelector("#buttonD");
+var endScreenEl = document.querySelector("#end-screen");
 
 // declare question array
-// var questions = [
-//     {
-//         quest: "Question 1",
-//         ans: {
-//             a: "wrong",
-//             b: "wrong",
-//             c: "correct",
-//             d: "wrong"
-//         },
-//         correct: "c"
-//     },
-//     {
-//         quest: "Question 2",
-//         ans: {
-//             a: "wrong",
-//             b: "wrong",
-//             c: "correct",
-//             d: "wrong"
-//         },
-//         correct: "c"
-//     }
-// ]
+var questions = [
+    {
+        quest: "Question 1",
+        ans: {
+            a: "wrong",
+            b: "wrong",
+            c: "right",
+            d: "wrong"
+        },
+        correct: "c"
+    },
+    {
+        quest: "Question 2",
+        ans: {
+            a: "incorrect",
+            b: "incorrect",
+            c: "correct",
+            d: "incorrect"
+        },
+        correct: "c"
+    }
+]
 
-// declare `questionIndex`
+// declare `index`
+var index = -1;
+var currentQuestion;
 
 // `startGame` function
 function startGame() {
     startTimer();
     // hide start screen element
-    document.getElementById("start-screen").style.visibility = "hidden";
-    document.getElementById("questions").style.visibility = "visible";
+    startScreenEl.classList.add("hidden");
+    // show question element
+    questionsEl.classList.remove("hidden");
+    nextQuestion();
 }
 
 // create `endGame` function
@@ -61,7 +70,9 @@ function startTimer() {
 }
 
 // create `answerQuestion` function    
-    
+function answerQuestion() {
+
+}
     // click event on all answer buttons
 
         // if incorrect subtract from score
@@ -69,7 +80,22 @@ function startTimer() {
     // continue to next question `nextQuestion()`
 
 // create `nextQuestion` function
-
+function nextQuestion() {
+    index = index + 1;
+    questionText.textContent = questions[index].quest;
+    buttonA.textContent = questions[index].ans.a;
+    buttonB.textContent = questions[index].ans.b;
+    buttonC.textContent = questions[index].ans.c;
+    buttonD.textContent = questions[index].ans.d;
+}
     // access current question from questions[questionIndex]
 
 startButton.addEventListener("click", startGame)
+
+buttonContainer.addEventListener("click", function(event) {
+    var element = event.target;
+
+    if (element.matches(".button")) {
+        nextQuestion();
+    }
+})
